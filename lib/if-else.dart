986c470423dd6e44d482beb61cbec453572c03f1
Rahat65 <m.rahatdrmc65@gmail.com>
2025-01-15 17:02:16 +0600

    Your commit message

---
+++ lib/if-else.dart
@@ -0,0 +1,19 @@
+main()
+{
+  double mark=50;
+  if(mark>=80 && mark<=100)
+    {
+      print('You are got A+');
+    }
+  else if(mark>=70 && mark<=79)
+  {
+    print('You are got A');
+  }
+  else if(mark>=60 && mark<=69)
+  {
+    print('You are got A-');
+  }
+  else {
+    print('unda');
+  }
+}
